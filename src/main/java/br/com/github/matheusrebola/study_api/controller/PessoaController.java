46007639a65de2bc6b38ba7c7258f8377d8fa717
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-02 22:44:10 -0300

    ultimo commit da noite

--- src/main/java/br/com/github/matheusrebola/study_api/controller/PessoaController.java
+++ src/main/java/br/com/github/matheusrebola/study_api/controller/PessoaController.java
@@ -1,9 +1,19 @@
 package br.com.github.matheusrebola.study_api.controller;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import br.com.github.matheusrebola.study_api.model.Pessoa;
+import br.com.github.matheusrebola.study_api.service.PessoaService;
+
+@RestController
+@RequestMapping("/pessoas")
 public class PessoaController {
+	private PessoaService pessoaService = new PessoaService();
+	
+	@GetMapping
 	public List<Pessoa> listAll(){
-		return null;
+		return pessoaService.list();
 	}
 }
