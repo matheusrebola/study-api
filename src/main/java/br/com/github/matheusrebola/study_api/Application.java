5b20c5c0dbfe25592f2432622b1b401054c9af80
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-02 21:48:07 -0300

    primeiro commit

---
+++ src/main/java/br/com/github/matheusrebola/study_api/Application.java
@@ -0,0 +1,25 @@
+package br.com.github.matheusrebola.study_api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application implements CommandLineRunner{
+	
+	static Logger LOG = LoggerFactory.getLogger(Application.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		LOG.info("estou testando o log");
+	}
+
+}
