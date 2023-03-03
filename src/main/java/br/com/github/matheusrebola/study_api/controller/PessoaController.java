59d83283a3bcf78110ea7b1de2dc98f35b8996c4
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-02 22:28:24 -0300

    terceito commit (eu adoro git)

--- src/main/java/br/com/github/matheusrebola/study_api/controller/PessoaController.java
+++ src/main/java/br/com/github/matheusrebola/study_api/controller/PessoaController.java
@@ -1,5 +1,9 @@
 package br.com.github.matheusrebola.study_api.controller;
+import java.util.List;
 
+import br.com.github.matheusrebola.study_api.model.Pessoa;
 public class PessoaController {
-
+	public List<Pessoa> listAll(){
+		return null;
+	}
 }
