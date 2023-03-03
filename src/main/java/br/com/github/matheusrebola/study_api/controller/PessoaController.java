c8bff9057c5bc2b56656428238df246364fd0229
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-02 22:14:21 -0300

    segundo commit da noite/ com tomcat

---
+++ src/main/java/br/com/github/matheusrebola/study_api/controller/PessoaController.java
@@ -0,0 +1,5 @@
+package br.com.github.matheusrebola.study_api.controller;
+
+public class PessoaController {
+
+}
---
+++ src/main/java/br/com/github/matheusrebola/study_api/model/Pessoa.java
@@ -0,0 +1,26 @@
+package br.com.github.matheusrebola.study_api.model;
+
+public class Pessoa {
+	private Long id;
+	private String nome;
+	private int idade;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+}
